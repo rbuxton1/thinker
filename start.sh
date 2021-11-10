0b0acfd70e673218d12f99b7152899c59e05c796
Ryan Buxton <rbuxton1@gmail.com>
2021-11-10 11:34:20 -0700

    oh yeah that too

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/ryan/thinker/
 screen -dmS node bash -c "node ~/thinker/index.js; exec sh"
 screen -dmS chromium bash -c "chromium-browser --start-fullscreen --diable-infobars --autoplay-policy=no-user-gesture-required http://localhost/animation; exec sh"
 #screen -dmS nodogsplash bash -c "sudo nodogsplash; exec sh"
