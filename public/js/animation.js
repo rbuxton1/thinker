5d89c4ba68bd878098cfeec77d8e66f567572a8b
Ryan Buxton <rbuxton1@gmail.com>
2021-11-10 11:51:20 -0700

    rookie

--- public/js/animation.js
+++ public/js/animation.js
@@ -26,26 +26,26 @@ $(document).ready(() => {
           console.log(data);
           switch (data) {
             case "skipped":
-              $("#container").fadeOut(250, () => {
+              $("#video").fadeOut(250, () => {
                 $("#video").attr("src", videos.skipped[niceRandom(videos.skipped.length)]);
                 $("#video").on("play", () => {
-                  $("#container").fadeIn(250);
+                  $("#video").fadeIn(250);
                 });
               });
               $.post("/state", {state: "idle"});
               break;
             case "liked":
-              $("#container").fadeOut(250, () => {
+              $("#video").fadeOut(250, () => {
                 $("#video").attr("src", videos.liked[niceRandom(videos.liked.length)]);
-                $("#container").fadeIn(350);
+                $("#video").fadeIn(350);
               });
               $.post("/state", {state: "idle"});
               break;
             case "idle":
               if($("#video").attr("src") != videos.idle)
-                $("#container").fadeOut(250, () => {
+                $("#video").fadeOut(250, () => {
                   $("#video").attr("src", videos.idle);
-                  $("#container").fadeIn(350);
+                  $("#video").fadeIn(350);
                 });
               break;
             default:
