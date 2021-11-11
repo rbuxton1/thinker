92b90a8101e1f9a22ebab4074ce6de7f1e898466
Ryan <rbuxton1@gmail.com>
2021-11-11 16:08:38 -0700

    no really now

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ryan/thinker/
 screen -dmS node bash -c "node ~/thinker/index.js; exec sh"
-screen -dmS chromium bash -c "chromium-browser --start-fullscreen --kiosk --autoplay-policy=no-user-gesture-required --app=http://localhost/animation; exec sh"
+screen -dmS chromium bash -c "chromium-browser --autoplay-policy=no-user-gesture-required --kiosk --app=http://localhost/animation; exec sh"
 #screen -dmS nodogsplash bash -c "sudo nodogsplash; exec sh"
